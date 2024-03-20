10b4b5f8585069f9b27454db898996020ecc14b3
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-20 23:54:26 +0900

    Variable 1

---
+++ java_training/src/ph1_basic/p02_variable.java
@@ -0,0 +1,22 @@
+package ph1_basic;
+
+public class p02_variable {
+	public static void main(String[] args) {
+		System.out.println("p02_variable");
+		// Khai bao bien
+		/*
+		 * Ctrl + D: Xoa di dong code muon xoa
+		 */
+//		int age;
+//		int year;
+//		char name;
+//		char city;
+//		int age, year;
+//		char name, city;
+		
+		// First name
+		char firstname;		// normal
+		char firstName;		// camelCase
+		char first_name;	// underscores
+	}
+}
