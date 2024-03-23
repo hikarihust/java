805e715d8fdb9169340309ae24b3f25cc9f44e27
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-23 16:31:31 +0900

    Toán tử gán

--- java_training/src/ph1_basic/p03_operator.java
+++ java_training/src/ph1_basic/p03_operator.java
@@ -2,6 +2,19 @@ package ph1_basic;
 
 public class p03_operator {
 	public static void main(String[] args) {
+		int number	= 20;
+		System.out.println("Start = " + number);
+		
+		number += 10;	// number	= number + 10;
+		number -= 10;	// number	= number - 10
+		number *= 10;	// number	= number * 10
+		number /= 10;	// number	= number / 10
+		number %= 10;	// number	= number % 10
+		System.out.println("End = " + number);
+	}
+	
+	// 001 Toan tu so hoc  + - * / %
+	public static void main001(String[] args) {
 		int numberOne	= 20;
 		int numberTwo	= 3;
 		int result;
