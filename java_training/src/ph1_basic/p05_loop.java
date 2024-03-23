e6f48b23f755592a8464990dd0a4bade41e0c1be
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-24 00:08:06 +0900

    Ex Bảng cửu chương _ Basic

--- java_training/src/ph1_basic/p05_loop.java
+++ java_training/src/ph1_basic/p05_loop.java
@@ -2,6 +2,34 @@ package ph1_basic;
 
 public class p05_loop {
 	public static void main(String[] args) {
+		int number = 9;
+		
+		// FOR
+		System.out.println("=============FOR=============");
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%d x %d = %d%n", number, i, number*i);
+		}
+		System.out.println("=============FOR=============");
+		// WHILE
+		System.out.println("=============WHILE=============");
+		int j = 1;
+		while(j <= 10){
+			System.out.printf("%d x %d = %d%n", number, j, number*j);
+			j++;
+		}
+		System.out.println("=============WHILE=============");
+		// DO WHILE
+		System.out.println("=============DO WHILE=============");
+		int k = 1;
+		do {
+			System.out.printf("%d x %d = %d%n", number, k, number*k);
+			k++;
+		}while(k <= 10);
+		System.out.println("=============DO WHILE=============");
+	}
+	
+	// 001 LOOP
+	public static void main001(String[] args) {
 		// FOR
 		for(int i = 1; i <= 10; i++){
 			System.out.println(i + " - FOR");
