e477c949f6adde37676a2c912c1cc77b54311dd8
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-25 02:00:37 +0900

    Overloading

--- java_training/src/ph2_oop/Main.java
+++ java_training/src/ph2_oop/Main.java
@@ -3,13 +3,59 @@ package ph2_oop;
 public class Main {
 
 	public static void main(String[] args) {
-		Student studentOne = new Student();
-
+		
+		Student studentOne	= new Student();
+		
+		//studentOne.setCode("S001");
+		studentOne.setCode(456);
+		
+		System.out.println("Code: " + studentOne.getCode());
+	}
+	
+	// 002 Setter + Getter + Method
+	public static void main002(String[] args) {
+		
+		Student studentOne	= new Student();
+		Student studentTwo	= new Student();
+		
+		// studentOne
 		studentOne.setBirthday(1994);
 		studentOne.setName("John");
 		studentOne.setCode("S001");
-		
 		studentOne.showInfo();
+		
+		// studentTwo
+		studentTwo.setBirthday(1990);
+		studentTwo.setName("Peter");
+		studentTwo.setCode("S002");
+		studentTwo.showInfo();
+	}
+	
+	// 001 Property
+	public static void main001(String[] args) {
+		
+		Student studentOne	= new Student();
+		Student studentTwo	= new Student();
+		
+		studentOne.birthday	= 1994;
+		studentOne.name		= "John";
+		studentOne.code		= "S001";
+		
+		studentTwo.birthday	= 1990;
+		studentTwo.name		= "Peter";
+		studentTwo.code		= "S002";
+		
+		System.out.println("Student Info: ");
+		System.out.println("- Name: " + studentOne.name);
+		System.out.println("- Code: " + studentOne.code);
+		System.out.println("- Birthday: " + studentOne.birthday);
+		
+		System.out.println("--------------------------");
+		
+		System.out.println("Student Info: ");
+		System.out.println("- Name: " + studentTwo.name);
+		System.out.println("- Code: " + studentTwo.code);
+		System.out.println("- Birthday: " + studentTwo.birthday);
 	}
 
 }
--- java_training/src/ph2_oop/Student.java
+++ java_training/src/ph2_oop/Student.java
@@ -6,21 +6,39 @@ public class Student {
 	public String name;
 	public String code;
 	public int birthday;
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getCode() {
 		return code;
 	}
+	
 	public void setCode(String code) {
 		this.code = code;
 	}
+	
+	public void setCode() {
+		this.code = "SV000";
+	}
+	
+	public void setCode(String str, int number) {
+		this.code = str + number;
+	}
+	
+	public void setCode(int number) {
+		this.code = "SV" + number;
+	}
+	
 	public int getBirthday() {
 		return birthday;
 	}
+	
 	public void setBirthday(int birthday) {
 		this.birthday = birthday;
 	}
