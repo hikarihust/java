372faba53e1e2320cafce9bd311630d9c5de43e8
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-24 12:36:26 +0900

    Shape 02

--- java_training/src/ph1_basic/p08_shape.java
+++ java_training/src/ph1_basic/p08_shape.java
@@ -3,6 +3,23 @@ package ph1_basic;
 public class p08_shape {
 	public static void main(String[] args) {
 		/*
+		 	*  *  *  *  *	number=5
+			*  *  *  *		number=4
+			*  *  *  		number=3
+			*  *  			number=2
+			*				number=1
+	 	*/
+		int number = 5;
+		while(number >= 1){
+			for(int i = 1; i <= number; i++) System.out.print("* ");
+			System.out.println();
+			number--;
+		}
+	}
+
+	// 001 Shape 1
+	public static void main001(String[] args) {
+		/*
 		 	*				 number=1 
 		 	*  *			 number=2
 		 	*  *  *			 number=3
