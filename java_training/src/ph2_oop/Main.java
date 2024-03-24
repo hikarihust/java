a39c0474bddfa6f02ed3eebe9c9b7808f5e7024a
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-03-25 00:41:36 +0900

    Getter & setter

--- java_training/src/ph2_oop/Main.java
+++ java_training/src/ph2_oop/Main.java
@@ -4,25 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Student studentOne = new Student();
-		Student studentTwo = new Student();
+
+		studentOne.setBirthday(1994);
+		studentOne.setName("John");
+		studentOne.setCode("S001");
 		
-		studentOne.birthday = 1994;
-		studentOne.name     = "John";
-		studentOne.code 	= "S001";
-		
-		studentTwo.birthday = 1995;
-		studentTwo.name     = "Jack";
-		studentTwo.code 	= "S002";
-		
-		System.out.println("Name: " + studentOne.name);
-		System.out.println("Code: " + studentOne.code);
-		System.out.println("Year: " + studentOne.birthday);
-		
-		System.out.println("------------------------");
-		
-		System.out.println("Name: " + studentTwo.name);
-		System.out.println("Code: " + studentTwo.code);
-		System.out.println("Year: " + studentTwo.birthday);
+		System.out.println("Name: " + studentOne.getName());
+		System.out.println("Code: " + studentOne.getCode());
+		System.out.println("Year: " + studentOne.getBirthday());
 	}
 
 }
--- java_training/src/ph2_oop/Student.java
+++ java_training/src/ph2_oop/Student.java
@@ -4,4 +4,22 @@ public class Student {
 	public String name;
 	public String code;
 	public int birthday;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public int getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
 }
