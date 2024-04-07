4dc990e10a3e211764272899fdfd831fdcbf2148
Vu Dinh Quang <vudinhquangk53@gmail.com>
2024-04-07 19:45:24 +0900

    Runtime & Checked

--- java_training/src/ph4_exception/Main.java
+++ java_training/src/ph4_exception/Main.java
@@ -1,8 +1,22 @@
 package ph4_exception;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Main {
 	public static void main(String[] args) {
-		study003();
+		study004();
+	}
+	
+	// Exception: Runtime + Checked
+	public static void study004() {
+		// Exception: Runtime Checked
+		try {
+			FileOutputStream fileObj	= new FileOutputStream("abc.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	// Try catch 
